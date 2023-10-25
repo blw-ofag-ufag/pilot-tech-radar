4312729a8c572462d9f61134753b1e8f2a05939b
Damian-Oswald <2133695@blv-pw-app-01.hse.begasoft.ch>
2023-10-25 17:50:30 +0200

    overhaul

---
+++ HEADER.md
@@ -0,0 +1,5 @@
+# Warm-up DBDD
+
+This repository contains the R script (`tech-radar.R`) used to create the technology radar pilot as a SVG file. The source data (`data.csv`) contains all relevant information for reproducing the result.
+
+![Technology radar.](technology-radar.png)
--- tech-radar.R
+++ tech-radar.R
@@ -4,8 +4,8 @@
 #' date: 2023-10-13
 #' ---
 
-#' Global parameters
-rotation <- 10 # in degrees [0,360]
+#' Fix random processes
+set.seed(1)
 
 #' Attach packages to search path.
 library(magrittr)
@@ -18,6 +18,9 @@ data <- data[!as.logical(rowSums(data[,2:3]=="")),]
 
 #' Change variable classes
 for (i in c("Sector","Status")) data[,i] %<>% tolower %<>% as.factor
+
+#' Sort data
+data <- data[with(data, order(Sector, Status)),]
 
 #' Information relevant for the dynamic plotting
 d <- nlevels(data[,"Status"])
@@ -38,32 +41,30 @@ for (sector in levels(data[,"Sector"])) {
 }
 
 #' Open an empty SVG file.
-png("technology-radar.png", width = 8, height = 12, res = 300, unit = "in")
+png("technology-radar.png", width = 12, height = 8, res = 300, unit = "in")
 
 #' Set graphical parameters
-par(bg = "gray20", mar = rep(3,4), fg = "gray90")
-layout(c(1,1,2))
+par(bg = "white", mar = rep(0,4))
 palette <- colorRampPalette(c("red","#517A7B"))
 
 #' Set the plotting plane
 plot(NA, xlim = c(-2,2), ylim = c(-1,2), axes = FALSE, xlab = "", ylab = "", asp = 1)
 
 #' Add the rings and sector segments
+width <- 35
 rings <- seq(-pi*(g/((g-1)*2)), pi*(g/((g-1)*2)),l=500)
-for (i in seq(1,2,0.25)) lines(sin(rings)*i, cos(rings)*i, lwd = 20, col = palette(7)[(i-1)*5+1])
-for (i in seq(-g/((g-1)*2),g/((g-1)*2),l=g+1)) lines(x = c(0, 3*sin(i*pi)), y = c(0, 3*cos(i*pi)), col = par()$bg, lwd = 20)
+for (i in seq(1,2,0.25)) lines(sin(rings)*i, cos(rings)*i, lwd = width, col = palette(7)[(i-1)*5+1])
+for (i in seq(-g/((g-1)*2),g/((g-1)*2),l=g+1)) lines(x = c(0, 3*sin(i*pi)), y = c(0, 3*cos(i*pi)), col = par()$bg, lwd = width)
 
 #' Add the numbered white points
-with(data, points((sin(x*pi - pi/2))*(y+1), cos(x*pi-pi/2)*(y+1), pch = 16, cex = 5, lwd = 3))
-with(data, text((sin(x*pi - pi/2))*(y+1), cos(x*pi-pi/2)*(y+1), 1:nrow(data), col = par()$bg, font = 2))
+with(data, points((sin(x*pi - pi/2))*(y+1), cos(x*pi-pi/2)*(y+1), pch = 21, cex = sqrt(Relevance)*2, lwd = sqrt(Relevance), bg = par()$bg))
+with(data, text((sin(x*pi - pi/2))*(y+1), cos(x*pi-pi/2)*(y+1), 1:nrow(data), cex = sqrt(Relevance)*0.6, col = par()$fg, font = 2))
 
-#' Add text
-for (i in 1:nrow(data)) {
-  color <- palette(6)[6-as.integer(data[i,"Status"])]
-  points(x = -2, y = -1.2 - i*0.1, cex = 2.2, xpd = NA, pch = 16)
-  text(x = -2, y = -1.2 - i*0.1, labels = i, xpd = NA, cex = 0.6, font = 2, col = par()$bg)
-  text(x = -1.97, y = - 1.215 - i*0.1, labels = bquote(bold(.(data[i,"Name"]))~~italic(.(data[i,"Description"]))), xpd = NA, pos = 4, font = 2, col = color)
-}
+#' Write the legend as text into the README file
+sink("README.md")
+readLines("HEADER.md") |> cat(sep="\n")
+for (i in 1:nrow(data)) cat("\n\n(", i, ") **", data[i,"Name"],":** ", data[i,"Description"], sep = "")
+sink()
 
 #' Add a title
 text(0, 2.4, "The FOAG Technology Radar (Pilot)", xpd = NA, cex = 3, font = 2)
